fefe9da7048aa6b23d312c5af164253e6e59a472
Mike Marcacci <mike.marcacci@thecontrolgroup.com>
2014-05-04 23:12:19 -0700

    Update endpoints

--- index.js
+++ index.js
@@ -8,34 +8,29 @@ var express = require('express'); require('express-namespace');
 var config = require('./config.json');
 var resources = {
 	db: require('./lib/utils/db.js')(config.db)
-}
+};
 
 var app = express();
 
-app.use(express.logger('dev')); // TODO: dev only
 
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 
-// add res.error and res.data methods
-app.use(require('./lib/utils/res.js'));
-
-// allow all CORS
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-	// intercept OPTIONS method
-	if ('OPTIONS' == req.method) {
-		res.send(200);
-	} else {
-		next();
-	}
+// configure app
+app.configure(function() {
+	app.use(express.logger());
+	app.use(require('./lib/middleware/cors.js')({}, resources));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(require('./lib/utils/res.js'));
+	app.use(require('./lib/middleware/auth.js')(config.auth, resources));
+	app.use(app.router);
+	app.use(express.static(__dirname + '/files'));
 });
 
 // add endpoints
 app.namespace(config.root, function(){
+
+	// add tokens endpoints
+	require('./lib/endpoints/tokens.js')(config, app, resources);
 
 	// add programs endpoints
 	require('./lib/endpoints/programs.js')(config, app, resources);
