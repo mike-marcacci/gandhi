bcbb35c5d21d991efcee423b066dbd11895db361
Mike Marcacci <mike.marcacci@gmail.com>
2014-09-13 18:02:27 -0700

    Upgrade rethinkdb to 1.14

--- test/tests/endpoints/users.js
+++ test/tests/endpoints/users.js
@@ -11,7 +11,7 @@ var assert = require('chai').assert;
 var request, fixtures;
 
 var blacklist = ['password', 'recovery_token'];
-var whitelist = ['id', 'email', 'name', 'href', 'admin', 'created','updated'];
+var whitelist = ['id', 'email', 'name', 'href', 'admin', 'created', 'updated'];
 
 before(function(){
 	request = global.setup.api;
@@ -30,10 +30,11 @@ describe('Users', function(){
 			})
 			.expect(201)
 			.end(function(err, res){
+				assert.isNull(err);
 				assert.isString(res.body.token);
 				adminToken = res.body.token;
 				adminId = jwt.decode(adminToken).sub;
-				done(err);
+				done();
 			});
 	});
 
@@ -48,13 +49,13 @@ describe('Users', function(){
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.token);
 					userToken = res.body.token;
 					userId = jwt.decode(userToken).sub;
-					done(err);
+					done();
 				});
 		});
-
 		it('rejects a misformatted user', function(done){
 			request
 				.post('/api/users')
@@ -62,11 +63,8 @@ describe('Users', function(){
 					cool: 'beans'
 				})
 				.expect(400)
-				.end(function(err, res){
-					done(err);
-				});
+				.end(done);
 		});
-
 		it('prevents email collisions', function(done){
 			request
 				.post('/api/users')
@@ -75,11 +73,8 @@ describe('Users', function(){
 					password: 'tim1234'
 				})
 				.expect(409)
-				.end(function(err, res){
-					done(err);
-				});
+				.end(done);
 		});
-
 		it('allows anonymous creation of a user', function(done){
 			request
 				.post('/api/users')
@@ -89,16 +84,16 @@ describe('Users', function(){
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.id);
 					ids.push(res.body.id);
 					assert.equal(res.body.email, 'emily.marcacci@test.gandhi.io');
 					blacklist.forEach(function(prop){
 						assert.notProperty(res.body, prop);
 					});
-					done(err);
+					done();
 				});
 		});
-
 		it('prevents anonymous creation of an admin user', function(done){
 			request
 				.post('/api/users')
@@ -109,11 +104,11 @@ describe('Users', function(){
 				})
 				.expect(403)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isUndefined(res.body.id);
-					done(err);
+					done();
 				});
 		});
-
 		it('prevents creation of an admin user by a non-admin user', function(done){
 			request
 				.post('/api/users')
@@ -125,11 +120,11 @@ describe('Users', function(){
 				})
 				.expect(403)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isUndefined(res.body.id);
-					done(err);
+					done();
 				});
 		});
-
 		it('allows creation of an admin user by an admin user', function(done){
 			request
 				.post('/api/users')
@@ -141,13 +136,14 @@ describe('Users', function(){
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.id);
 					ids.push(res.body.id);
 					assert.equal(res.body.email, 'solène.clavel@test.gandhi.io');
 					blacklist.forEach(function(prop){
 						assert.notProperty(res.body, prop);
 					});
-					done(err);
+					done();
 				});
 		});
 	});
@@ -163,10 +159,11 @@ describe('Users', function(){
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.token);
 					userToken = res.body.token;
 					userId = jwt.decode(userToken).sub;
-					done(err);
+					done();
 				});
 		});
 
@@ -176,8 +173,9 @@ describe('Users', function(){
 					.get('/api/users')
 					.expect(401)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isNotArray(res.body);
-						done(err);
+						done();
 					});
 			});
 			it('returns list of users', function(done){
@@ -186,9 +184,10 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isArray(res.body);
 						assert.lengthOf(res.body, Math.min(50, fixtures.length + ids.length));
-						done(err);
+						done();
 					});
 			});
 			it('accepts per_page parameters', function(done){
@@ -197,12 +196,13 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isArray(res.body);
 						assert.lengthOf(res.body, 5);
 						var links = li.parse(res.headers.link);
 						assert.equal(links.next, '/api/users?per_page=5&page=2');
 						assert.equal(links.last, '/api/users?per_page=5&page='+Math.ceil((fixtures.length + ids.length) / 5));
-						done(err);
+						done();
 					});
 			});
 			it('accepts page parameters', function(done){
@@ -211,12 +211,13 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isArray(res.body);
 						assert.lengthOf(res.body, 5);
 						var links = li.parse(res.headers.link);
 						assert.equal(links.first, '/api/users?per_page=5&page=1');
 						assert.equal(links.prev, '/api/users?per_page=5&page=1');
-						done(err);
+						done();
 					});
 			});
 			it('only includes whitelisted fields to unaffiliated users', function(done){
@@ -225,6 +226,7 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isArray(res.body);
 						_.each(res.body, function(user){
 							whitelist.forEach(function(prop){
@@ -233,7 +235,7 @@ describe('Users', function(){
 							if(user.email != 'tim.marcacci@test.gandhi.io')
 								assert.lengthOf(Object.keys(user), whitelist.length, 'contains properties in addition to whitelist');
 						})
-						done(err);
+						done();
 					});
 			});
 			it('includes all non-blacklisted fields to self', function(done){
@@ -242,6 +244,7 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isArray(res.body);
 						var user = _.find(res.body, {email: 'tim.marcacci@test.gandhi.io'});
 						blacklist.forEach(function(prop){
@@ -250,7 +253,7 @@ describe('Users', function(){
 
 						// TODO: test for other properties
 
-						done(err);
+						done();
 					});
 			});
 			it('includes all non-blacklisted fields to admin users', function(done){
@@ -259,6 +262,7 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + adminToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isArray(res.body);
 						var user = _.find(res.body, {email: 'tim.marcacci@test.gandhi.io'});
 						blacklist.forEach(function(prop){
@@ -267,7 +271,7 @@ describe('Users', function(){
 
 						// TODO: test for other properties
 
-						done(err);
+						done();
 					});
 			});
 		});
@@ -278,8 +282,9 @@ describe('Users', function(){
 					.get('/api/users/' + userId)
 					.expect(401)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.isNotArray(res.body);
-						done(err);
+						done();
 					});
 			});
 			it('returns the correct user', function(done){
@@ -288,9 +293,10 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.property(res.body, 'id');
 						assert.equal(res.body.id, userId);
-						done(err);
+						done();
 					});
 			});
 			it('only includes whitelisted fields to unaffiliated users', function(done){
@@ -299,12 +305,13 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						whitelist.forEach(function(prop){
 							assert.property(res.body, prop);
 						}, 'missing whitelist property');
 						assert.equal(res.body.id, adminId);
 						assert.lengthOf(Object.keys(res.body), whitelist.length, 'contains properties in addition to whitelist');
-						done(err);
+						done();
 					});
 			});
 			it('includes all non-blacklisted fields to self', function(done){
@@ -313,6 +320,7 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + userToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.property(res.body, 'id');
 						assert.equal(res.body.id, userId);
 						blacklist.forEach(function(prop){
@@ -321,7 +329,7 @@ describe('Users', function(){
 
 						// TODO: test for other properties
 
-						done(err);
+						done();
 					});
 			});
 			it('includes all non-blacklisted fields to admin users', function(done){
@@ -330,6 +338,7 @@ describe('Users', function(){
 					.set('Authorization', 'Bearer ' + adminToken)
 					.expect(200)
 					.end(function(err, res){
+						assert.isNull(err);
 						assert.property(res.body, 'id');
 						assert.equal(res.body.id, userId);
 						blacklist.forEach(function(prop){
@@ -338,7 +347,7 @@ describe('Users', function(){
 
 						// TODO: test for other properties
 
-						done(err);
+						done();
 					});
 			});
 		});
@@ -355,47 +364,163 @@ describe('Users', function(){
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.token);
 					userToken = res.body.token;
 					userId = jwt.decode(userToken).sub;
-					done(err);
+					done();
 				});
 		});
 
-		it.skip('rejects an anonymous update', function(){});
-		it.skip('rejects an update from an unaffiliated user', function(){});
-		it.skip('processes an update from self', function(){});
-		it.skip('processes an update from an admin user', function(){});
-		it.skip('rejects promition of a user to admin by a non-admin user', function(){});
-		it.skip('rejects a misformatted update', function(){});
+		it('rejects an anonymous request', function(done){
+			request
+				.patch('/api/users/' + userId)
+				.send({
+					name: 'Woops!'
+				})
+				.expect(401)
+				.end(function(err, res){
+					assert.isNull(err);
+					done();
+				});
+		});
+		it('rejects an update from an unaffiliated, non-admin user', function(done){
+			request
+				.patch('/api/users/' + adminId)
+				.set('Authorization', 'Bearer ' + userToken)
+				.send({
+					name: 'Woops!'
+				})
+				.expect(403)
+				.end(done);
+		});
+		it('processes an update from self', function(done){
+			request
+				.patch('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + userToken)
+				.send({
+					name: 'Emily Shafer Marcacci'
+				})
+				.expect(200)
+				.end(function(err, res){
+					assert.isNull(err);
+					assert.equal(res.body.id, userId);
+					assert.equal(res.body.name, 'Emily Shafer Marcacci');
+					done();
+				});
+		});
+		it('processes an update from an admin user', function(done){
+			request
+				.patch('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + adminToken)
+				.send({
+					name: 'Emily S. Marcacci'
+				})
+				.expect(200)
+				.end(function(err, res){
+					assert.isNull(err);
+					assert.equal(res.body.id, userId);
+					assert.equal(res.body.name, 'Emily S. Marcacci');
+					done();
+				});
+		});
+		it('rejects promition of a user to admin by a non-admin user', function(done){
+			request
+				.patch('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + userToken)
+				.send({
+					admin: true
+				})
+				.expect(403)
+				.end(done);
+		});
+		it('rejects a misformatted update', function(done){
+			request
+				.patch('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + userToken)
+				.send({
+					foo: 'bar'
+				})
+				.expect(400)
+				.end(done);
+		});
 		it.skip('rejects an update to blacklisted properties', function(){});
 	});
 
 	describe('#replace', function(){
-		var userToken, userId;
+		var userToken, userId, user;
 		before(function(done){
 			request
 				.post('/api/tokens')
 				.send({
-					email: 'solène.clavel@test.gandhi.io',
-					password: 'solène1234'
+					email: 'emily.marcacci@test.gandhi.io',
+					password: 'emily1234'
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.token);
 					userToken = res.body.token;
 					userId = jwt.decode(userToken).sub;
-					done(err);
+					return done();
+				});
+		});
+		before(function(done){
+			request
+				.get('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + userToken)
+				.end(function(err, res){
+					assert.isNull(err);
+					user = res.body;
+					return done();
 				});
 		});
 
-		it.skip('rejects an anonymous replace', function(){});
-		it.skip('rejects a replace from an unaffiliated user', function(){});
-		it.skip('processes a replace from self', function(){});
-		it.skip('processes a replace from an admin user', function(){});
-		it.skip('rejects promition of a user to admin by a non-admin user', function(){});
-		it.skip('rejects a misformatted replace', function(){});
-		it.skip('rejects a replace to blacklisted properties', function(){});
+		it('rejects an anonymous replace', function(done){
+			request
+				.put('/api/users/' + userId)
+				.send(_.extend({}, user, {
+					name: 'Woops!'
+				}))
+				.expect(401)
+				.end(done);
+		});
+		it('rejects a replace from a non-admin user', function(done){
+			request
+				.put('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + userToken)
+				.send(_.extend({}, user, {
+					name: 'Woops!'
+				}))
+				.expect(403)
+				.end(done);
+		});
+		it('processes a replace from an admin user', function(done){
+			request
+				.put('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + adminToken)
+				.send(_.extend({}, user, {
+					name: 'Emily Martha Shafer Marcacci'
+				}))
+				.expect(200)
+				.end(function(err, res){
+					assert.isNull(err);
+					assert.equal(res.body.id, userId);
+					assert.equal(res.body.name, 'Emily Martha Shafer Marcacci');
+					done();
+				});
+		});
+		it.skip('rejects a misformatted replace', function(done){
+			request
+				.put('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + adminToken)
+				.send(_.extend({}, user, {
+					foo: 'bar'
+				}))
+				.expect(400)
+				.end(done);
+		});
+		it.skip('rejects a replace of blacklisted properties', function(){});
 	});
 
 	describe('#delete', function(){
@@ -409,17 +534,45 @@ describe('Users', function(){
 				})
 				.expect(201)
 				.end(function(err, res){
+					assert.isNull(err);
 					assert.isString(res.body.token);
 					userToken = res.body.token;
 					userId = jwt.decode(userToken).sub;
-					done(err);
+					done();
 				});
 		});
 
-		it.skip('rejects an anonymous delete', function(){});
-		it.skip('rejects a delete from an unaffiliated user', function(){});
-		it.skip('rejects a delete from self', function(){});
-		it.skip('processes a delete from an admin user', function(){});
+		it('rejects an anonymous delete', function(done){
+			request
+				.delete('/api/users/' + userId)
+				.expect(401)
+				.end(done);
+		});
+		it('rejects a delete by a non-admin user', function(done){
+			request
+				.delete('/api/users/' + userToken)
+				.set('Authorization', 'Bearer ' + userToken)
+				.expect(403)
+				.end(done);
+		});
+		it('rejects a delete of self by an admin user', function(done){
+			request
+				.delete('/api/users/' + adminId)
+				.set('Authorization', 'Bearer ' + adminToken)
+				.expect(400)
+				.end(done);
+		});
+		it('processes a delete of another user by an admin user', function(done){
+			request
+				.delete('/api/users/' + userId)
+				.set('Authorization', 'Bearer ' + adminToken)
+				.expect(200)
+				.end(function(err, res){
+					assert.isNull(err);
+					assert.equal(res.body.id, userId);
+					done();
+				});
+		});
 	});
 
 	// remove any users we just created
