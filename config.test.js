fa5dc4260fedc12724d8961c0a926610d340068b
Mike Marcacci <mike.marcacci@gmail.com>
2015-07-05 10:20:55 -0700

    Testing wercker...

--- config.test.js
+++ config.test.js
@@ -16,7 +16,7 @@ module.exports = {
 		timeout: 30000
 	},
 	redis: {
-		host: process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1',
+		host: process.env.REDIS_PORT_6379_TCP_ADDR || 'localhost',
 		port: process.env.REDIS_PORT_6379_TCP_PORT || 6379
 	},
 	lock: {
