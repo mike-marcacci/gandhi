4780e32c5d4269496812e4212a356139727b0a3d
Mike Marcacci <mike.marcacci@gmail.com>
2015-09-02 23:36:42 -0700

    Update UI for triggers

---
+++ migrations/v0.3.0.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var r = require('rethinkdb');
+var q = require('q');
+
+var host = '127.0.0.1';
+var db = 'gandhi';
+
+// grab command line agrs
+for (var i = 2; i < process.argv.length; i++) {
+	if(process.argv[i] === '--host'){
+		i++; host = process.argv[i];
+	}
+
+	else if(process.argv[i] === '--db'){
+		i++; db = process.argv[i];
+	}
+}
+
+
+r.connect({host: host, db: db}).then(function(conn){
+
+	var tasks = [];
+
+	// Cycles
+	// ------
+	tasks.push(r.table('cycles').replace(function(cycle){
+		return cycle.merge({
+			triggers: cycle('triggers').coerceTo('array').map(function(triggerKV){
+				return [triggerKV.nth(0), triggerKV.nth(1).merge({
+					description: triggerKV.nth(1)('description').default(''),
+				}).without('single')];
+			}).coerceTo('object')
+		});
+	}).run(conn));
+
+	// run all the tasks
+	q.all(tasks).then(function(res){
+		console.log('success:', JSON.stringify(res, null, 2));
+		conn.close();
+	}, function(err){
+		console.error(err);
+		conn.close();
+	});
+});
